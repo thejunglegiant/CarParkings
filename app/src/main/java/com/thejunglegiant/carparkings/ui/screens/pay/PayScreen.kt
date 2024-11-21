9f85bc4fe06da53734a91b60e5fc161673c21099
o.morozov <oleksii.m02@gmail.com>
2024-11-22 00:41:09 +0200

    added timer

--- app/src/main/java/com/thejunglegiant/carparkings/ui/screens/pay/PayScreen.kt
+++ app/src/main/java/com/thejunglegiant/carparkings/ui/screens/pay/PayScreen.kt
@@ -1,44 +1,96 @@
 package com.thejunglegiant.carparkings.ui.screens.pay
 
 import android.content.Intent
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.min
+import androidx.compose.ui.unit.sp
 import com.thejunglegiant.carparkings.MainActivity
 import com.thejunglegiant.carparkings.ui.getActivity
-import com.thejunglegiant.carparkings.ui.theme.PrimaryColor
+import kotlinx.coroutines.delay
 
 @Composable
 fun PayScreen() {
     val activity = LocalContext.current.getActivity() ?: return
+    // Timer state
+    var currentTime by remember { mutableStateOf(2 * 60 * 60) }
+    var timeStr by remember { mutableStateOf("02:00") }
 
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+    // Start countdown timer
+    LaunchedEffect(currentTime) {
+        if(currentTime > 0) {
+            delay(1000L)
+            currentTime -= 1
+            val hours = currentTime / 3600
+            val minutes = (currentTime % 3600) / 60
+            timeStr = String.format("%02d:%02d", hours, minutes)
+        }
+    }
+
+    // Screen Layout
+    Scaffold { paddingValues ->
         Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "Оплата пройшла успішно!")
-            Spacer(modifier = Modifier.height(8.dp))
+            // Thank You Text
+            Text(
+                text = "Дякуємо за оплату!",
+                style = MaterialTheme.typography.displaySmall,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(horizontal = 16.dp)
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+
+            // Big Timer
+            Text(
+                text = timeStr,
+                style = MaterialTheme.typography.displayLarge.copy(fontSize = 96.sp),
+                fontWeight = FontWeight.Bold,
+                color = Color.Black,
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+
             Button(
                 onClick = {
                     val intent = Intent(activity, MainActivity::class.java)
                     activity.startActivity(intent)
                     Runtime.getRuntime().exit(0)
                 },
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = PrimaryColor,
-                ),
+                colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary),
+                shape = MaterialTheme.shapes.medium
             ) {
-                Text(text = "Головна")
+                Text(
+                    text = "Повернутися на головну",
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = MaterialTheme.colorScheme.onPrimary
+                )
             }
         }
     }
